78548abc9cddc38bfd0e6ef199652ac556d6d891
Trevon <trevon.ceaser@ibm.com>
2023-12-07 12:58:07 -0600

    reset

---
+++ src/main/java/com/example/backendupdated/BackendupdatedApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendupdated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendupdatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendupdatedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendupdated/BackendupdatedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendupdated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendupdatedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
